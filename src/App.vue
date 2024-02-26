307c660673b1b058b066c359c3424c6a6aebf2fc
Jair-Mast3r <hiram.ramirezsn@yahoo.com>
2024-02-26 16:35:26 -0600

    Changing color of text

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,28 @@ import {ref} from "vue";
 const count = ref(0);
 
 function increment() {
+    //count.value = count.value * 100000000000000000000 + 1;
     count.value++;
+}
+function decrement() {
+    //count.value = count.value * 100000000000000000000 + 1;
+    count.value--;
 }
 </script>
 
 <template>
 <div>
-    <h3>El contador actual es: {{count}}</h3>
+    <h3 :class="{red: count > 10, green : count < 0}">El contador actual es: {{count}}</h3>
     <button @click="increment" type="button">Incrementar</button>
+    <button @click="decrement" type="button">Decrementar</button>
 </div>
 </template>
 
 <style scoped>
-
+.red{
+    color:red;
+}
+.green{
+    color:green;
+}
 </style>
