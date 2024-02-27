06de15c5ce093c8ef14141024f2ae6dbc0ee09a6
Jair-Mast3r <hiram.ramirezsn@yahoo.com>
2024-02-27 15:41:23 -0600

    Adding html login components

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,21 @@
 </script>
 
 <template>
-
+    <div>
+        <h1>Login</h1>
+        <form>
+            <div>
+                <label for="">Email</label>
+                <input type="text" name="email">
+            </div>
+            <div>
+                <label for="password">Contraseña</label>
+                <input type="text" name="password">
+            </div>
+        </form>
+    </div>
 </template>
 
 
 
-<style scoped>
-
-</style>
+<style scoped></style>
